7915755d544843c749d8e2bd95a38cc24f6397b3
Brianoyaro <brianoyaro2000@yahoo.com>
2023-10-02 18:14:38 +0000

    efficiency of 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,25 +8,11 @@
  */
 int check_cycle(listint_t *list)
 {
-	/*listint_t *slow, *fast;
-
-	slow = malloc(sizeof(listint_t));
-	fast = malloc(sizeof(listint_t));
-	fast = list;
-	slow = list;
-	while (slow && fast->next->next && slow->next)
-	{
-		fast = fast->next->next;
-		if (slow == fast)
-			return (1);
-		slow = slow->next;
-	}
-	return (0);*/
 	listint_t *fast;
 
-	/*fast = malloc(sizeof(listint_t));*/
 	fast = list;
-	while (list && list->next && fast->next->next)
+	/*while (list && list->next && fast->next->next)*/
+	while (fast && fast->next)
 	{
 		fast = fast->next->next;
 		if (list == fast->next->next)
