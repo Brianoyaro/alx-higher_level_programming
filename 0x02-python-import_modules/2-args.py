7a21902849efc0ab6b3d74e59777211541b5e9c2
Brianoyaro <brianoyaro2000@yahoo.com>
2023-10-05 10:51:17 +0000

    _import__

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-import sys
-arguments = sys.argv
-length = len(arguments)
-if (length == 1):
-    print("0 arguments.")
-elif (length == 2):
-    print("1 argument:")
-    print("{}: {}".format(length,arguments[1]))
-elif (length > 2):
-    print("{} arguments:".format(length - 1))
-    for i, value in enumerate(arguments):
-        if (i == 0):
-            continue
-        print("{}: {}".format(i, value))
+if __name__ == "__main__":
+    import sys
+    arguments = sys.argv
+    length = len(arguments)
+    if (length == 1):
+        print("0 arguments.")
+    elif (length == 2):
+        print("1 argument:")
+        print("{}: {}".format(length,arguments[1]))
+    elif (length > 2):
+        print("{} arguments:".format(length - 1))
+        for i, value in enumerate(arguments):
+            if (i == 0):
+                continue
+            print("{}: {}".format(i, value))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import sys
-args = sys.argv
-summ = 0
-for i, value in enumerate(args):
-    if (i == 0):
-        continue
-    summ += int(value)
-print(summ)
+if __name__ == "__main__":
+    import sys
+    args = sys.argv
+    summ = 0
+    for i, value in enumerate(args):
+        if (i == 0):
+            continue
+        summ += int(value)
+    print(summ)
