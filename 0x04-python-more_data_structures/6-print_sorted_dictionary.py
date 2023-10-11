8930034a4af6f743b41b9ffb5e831d38fbaeb2a1
Brianoyaro <brianoyaro2000@yahoo.com>
2023-10-11 15:23:54 +0000

    minor bugs

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    sorted_list = list(sorted([key for key in a_dictionary.keys()]))
-    length = len(a_dictionary)
-    for i in range(length):
-        value = a_dictionary.get(sorted_list[i])
-        print("{} : {}".format(sorted_list[i], value))
+    for key,value in sorted(a_dictionary.items()):
+        print("{} : {}".format(key, value))
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,6 @@
 def multiply_by_2(a_dictionary):
     new = {}
     for key,value in a_dictionary.items():
-        new.update({key : value * 2})
+        buf = value * 2
+        new.update({key : buf})
     return new
