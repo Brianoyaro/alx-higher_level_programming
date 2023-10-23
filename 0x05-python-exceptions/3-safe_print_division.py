3e928eb52a6864af470d9843aedf9927845ab18d
Brianoyaro <brianoyaro2000@yahoo.com>
2023-10-23 14:07:03 +0300

    3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,5 +5,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         val = None
     finally:
-        print("Inside result: {}", val)
+        print("Inside result: {}".format(val))
         return val
