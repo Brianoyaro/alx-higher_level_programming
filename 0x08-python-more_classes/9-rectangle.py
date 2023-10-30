79a0e8857e0135a1ded624eb0d8e1fa39b679fe8
Brianoyaro <brianoyaro2000@yahoo.com>
2023-10-30 13:18:55 +0300

    8

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -75,10 +75,9 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return val
         else:
-            for i in range(self.__height):
-                for j in range(self.__width):
-                    print(self.print_symbol, end='')
-                print()
+            for i in range(self.__height - 1):
+                val += (self.print_symbol * self.__width) + '\n'
+            val += (self.print_symbol * self.__width)
             return val
 
     def __repr__(self):
