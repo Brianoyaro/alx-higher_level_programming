c0cb0e819e2cba2f2a68423368d6ad86d7159240
Brianoyaro <brianoyaro2000@yahoo.com>
2023-11-06 15:30:29 +0300

    9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -12,10 +12,12 @@ class Rectangle(mod):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
+
     def area(self):
         """returns area of rectangle
         """
         return self.__width * self.__height
+
     def __str__(self):
         """returns a printable format string
         """
