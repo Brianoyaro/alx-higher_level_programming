f479212ed31c7200302ba537f6dc2b41109451fe
Brianoyaro <brianoyaro2000@yahoo.com>
2023-11-06 17:43:21 +0300

    1, 7

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,12 +8,6 @@ class MyList(list):
 
     iherits from class list
     """
-
-    def __init__(self):
-        """initialises each object instance
-        """
-        super().__init__(self)
-
     def print_sorted(self):
         """prints each class instance in sorted order
         """
