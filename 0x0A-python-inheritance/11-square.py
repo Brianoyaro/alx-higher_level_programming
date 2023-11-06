9e8778338ad1a94ed67cf3cb4362172e0cbff4b0
Brianoyaro <brianoyaro2000@yahoo.com>
2023-11-06 15:48:26 +0300

    10

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""square module
+"""
+mod = __import__('9-rectangle').Rectangle
+
+
+class Square(mod):
+    """square class
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """returns area of square
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        """returns a nicely printable string
+        """
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
