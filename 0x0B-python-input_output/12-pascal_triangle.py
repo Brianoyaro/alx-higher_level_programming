2c3c7142c5117f2cbbabe68b0601a78e5786af49
Brianoyaro <brianoyaro2000@yahoo.com>
2023-11-07 21:50:38 +0300

    pascal

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -13,5 +13,17 @@ def pascal_triangle(n):
         return [1]
     if n == 2:
         return [[1],[1, 1]]
-    
+    else:
+        final_list = [[1], [1, 1]]
+        lis = [1, 1]
+        for y in range(n - 2):
+            new = [1]
+            for i in range(len(lis) - 1):
+                left = les[i]
+                right = lis[i + 1]
+                new.append(left + right)
+            new.append(1)
+            lis = new
+            final_list.append(lis)
+        return final_list
 
