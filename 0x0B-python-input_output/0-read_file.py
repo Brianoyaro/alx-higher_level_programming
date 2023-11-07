9ddc8d5d708ebf4f64a0d7570f08bc40eb8c3683
Brianoyaro <brianoyaro2000@yahoo.com>
2023-11-07 17:54:11 +0300

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,5 @@ def read_file(filename=""):
     """prints contents of file filename
     """
     with open(filename, encoding='utf-8') as f:
-        print(f.read())
+        for line in f:
+            print(line, end="")
