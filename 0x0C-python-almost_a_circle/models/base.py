ddf3ba52682aaf6e9ba9537a7f26b25268c18d02
Brianoyaro <brianoyaro2000@yahoo.com>
2023-11-11 08:14:15 +0300

    fourth commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,3 +25,30 @@ class Base:
             return json.dumps([])
         else:
             return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """eturns the list of the JSON string representation json_string
+        """
+        if json_string is None or not json_string:
+            return []
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set
+        """
+        dummy = cls(1, 1, 1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        dummy = cls(1, 2, 3)
+        if dummy.width == dummy.height:
+            filename = 'Square.json'
+        else:
+            filename = 'Rectangle.json'
+        with open(filename, mode='w', encoding='utf-8') as f:
+            f.write(cls.to_json_string(list_objs))
