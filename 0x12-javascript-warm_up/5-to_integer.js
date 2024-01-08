f0e252a52e4c599f3cde76c6322a2ef0592ec3ed
Brianoyaro <brianoyaro2000@yahoo.com>
2024-01-08 22:31:41 +0300

    5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const args = process.argv;
 const num = parseInt(args[2]);
 if ((num % 1) === 0) {
-  console.log('My number: ', num);
+  console.log('My number:', num);
 } else {
   console.log('Not a number');
 }
