4d6824ee8f8411cdee05d8cd006f7b7f0e9de8f7
Brianoyaro <brianoyaro2000@yahoo.com>
2024-01-12 13:02:07 +0300

    7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,14 +2,15 @@
 """7-model_state_fetch_all.py module"""
 from state_model import Base, State
 import sys
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-Base.metadata.create_all(engine)
-"""create a new sessioni to manage your tranasction"""
-session = Sessio(engine)
-for state in session.query(States).order_by(state_id).all():
-    print(state)
-"""close the session"""
-session.close()
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    """create a new sessioni to manage your tranasction"""
+    session = Session(engine)
+    for state in session.query(States).order_by(state_id).all():
+        print(state)
+    """close the session"""
+    session.close()
