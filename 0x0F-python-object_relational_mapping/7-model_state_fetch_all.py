677726d5abd41d1142d7bed41c180aa82db4f88b
Brianoyaro <brianoyaro2000@yahoo.com>
2024-01-12 13:26:09 +0300

    7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,6 +2,7 @@
 """7-model_state_fetch_all.py module"""
 from model_state import Base, State
 import sys
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
 
 
@@ -9,10 +10,11 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    """create a new session to manage your tranasction"""
+    """create a session factory"""
     Session = sessionmaker(bind=engine)
+    """create a new session to manage your tranasction"""
     session = Session()
-    for state in session.query(States).order_by(state_id).all():
+    for state in session.query(State).order_by(state_id).all():
         print("{}: {}".fomat(state.id, state.name))
     """close the session"""
     session.close()
