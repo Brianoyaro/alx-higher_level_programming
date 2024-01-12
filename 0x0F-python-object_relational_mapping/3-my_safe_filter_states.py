2215b981e902272d27071bb19bd764ee61367420
Brianoyaro <brianoyaro2000@yahoo.com>
2024-01-12 06:08:33 +0300

    initial commit

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""odule showcasing usage of mysqldb
+"""
+import MySQLdb
+import sys
+
+
+arg = sys.argv
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost",
+            port=3306,
+            user=arg[1],
+            passwd=arg[2],
+            db=arg[3])
+"""create a connection to the database"""
+curr = conn.cursor
+name = arg[4]
+query = "SELECT * FROM states WHERE name = %s"
+curr.execute(query, (name,))
+"""fetch query results"""
+rows = curr.fetchall()
+for row in rows:
+    print(row)
+"""close cursor connection"""
+curr.close()
+"""close database connection"""
+conn.close()
