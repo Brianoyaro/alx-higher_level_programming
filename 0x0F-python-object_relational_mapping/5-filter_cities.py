cdd633a81c14f4eeb74cee6b3f08ffc17b0b8b89
Brianoyaro <brianoyaro2000@yahoo.com>
2024-01-12 09:20:56 +0300

    5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
             FROM cities\
             INNER JOIN states\
             ON cities.state_id=states.id\
-            WHERE states.name = %s\
+            WHERE states.name LIKE %s\
             ORDER BY cities.id ASC;"
     curr.execute(query, (name,))
     """fetch query results"""
