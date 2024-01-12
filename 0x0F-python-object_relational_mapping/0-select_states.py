5a305e227f0f1713d39153aa5d35bfcd258ee6b7
Brianoyaro <brianoyaro2000@yahoo.com>
2024-01-12 08:26:34 +0300

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,14 +9,14 @@ arg = sys.argv
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost",
                            port=3306, user=arg[1], passwd=arg[2], db=arg[3])
-"""create a connection to the database"""
-curr = conn.cursor
-curr.execute("SELECT * FROM states ORDER BY states.id ASC")
-"""fetch query results"""
-rows = curr.fetchall()
-for row in rows:
-    print(row)
-"""close cursor connection"""
-curr.close()
-"""close database connection"""
-conn.close()
+    """create a connection to the database"""
+    curr = conn.cursor
+    curr.execute("SELECT * FROM states ORDER BY states.id ASC")
+    """fetch query results"""
+    rows = curr.fetchall()
+    for row in rows:
+        print(row)
+    """close cursor connection"""
+    curr.close()
+    """close database connection"""
+    conn.close()
