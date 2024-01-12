482e9e6f75d69c01965ebe518bd3d7056fe88054
Brianoyaro <brianoyaro2000@yahoo.com>
2024-01-12 14:40:40 +0300

    12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""7-model_state_fetch_all.py module"""
+from model_state import Base, State
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    """create a session factory"""
+    Session = sessionmaker(bind=engine)
+    """create a new session to manage your tranasction"""
+    session = Session()
+    """add a new entry"""
+    new = session.query(State).filter_by(id=2).all()
+    new.name = 'New Mexico'
+    session.commit()
+
+    """close the session"""
+    session.close()
