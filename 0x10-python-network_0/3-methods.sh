156b594bce6b341c3b3ff9fd6604ff271f866f04
Brianoyaro <brianoyaro2000@yahoo.com>
2024-01-25 14:24:50 +0300

    3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -s -L -X OPTIONS "$1" | grep "Allow:" | tr -d '\r' | cut -d " " -f2-
+curl -sI -L -X OPTIONS "$1" | grep "Allow:" | tr -d '\r' | cut -d " " -f2-
