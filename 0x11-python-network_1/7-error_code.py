41ebe4955400a0406cdf27328ba4007b6e504760
Brianoyaro <brianoyaro2000@yahoo.com>
2024-01-26 11:40:37 +0300

    7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""handles request response errors"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    try:
+        response = requests.get(sys.argv[1])
+        print(response.text)
+    except requests.exceptions as e:
+        if e.status_code > 400:
+            print("Error code:" e.status_code)
