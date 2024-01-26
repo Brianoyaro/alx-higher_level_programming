d342dd5387697dd37bae1149df79bdd4c626747e
Brianoyaro <brianoyaro2000@yahoo.com>
2024-01-26 11:31:48 +0300

    6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,6 +6,5 @@ import requests
 
 if __name__ == "__main__":
     value = {"email": sys.argv[2]}
-    body = requests.post(sys.argv[1], data=value)
-    body = requests.get(sys.argv[1])
+    body = requests.post(sys.argv[1], value)
     print(body.text)
