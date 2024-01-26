8b547e0ddc47f2a6ef1646d7838c7cd87f286f13
Brianoyaro <brianoyaro2000@yahoo.com>
2024-01-26 13:57:33 +0300

    8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""searches using given parameter"""
+import requests
+import sys
+
+
+if __name__ == "__main++":
+    url = "http://0.0.0.0:5000/search_user"
+    if len(sys.argv) == 1:
+        value(q: "")
+    else:
+        value(q: sys.argv[1])
+    response = requests.post(url, value)
+    if 'application/json' in response.headers.get('Content-Type', ''):
+        if response.text:
+            print("[{}] {}".format(response.get('id'), response.get('name')))
+        else:
+            print("No result")
+    else:
+        print("Not a valid JSON")
+
